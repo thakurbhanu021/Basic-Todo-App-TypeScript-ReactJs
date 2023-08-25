9642121ed6aebefccc06f922c86ae9267765f15b
thakurbhanu021 <thakurbhanu021@gmail.com>
2023-08-25 13:34:18 +0530

    setting up Context for todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
-import { useState } from "react";
-
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 
-import Todo from "./models/todo";
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  const addTodoHandler = (todoText: string) => {
-    const newTodo = new Todo(todoText);
-
-    setTodos((prev) => {
-      return prev.concat(newTodo);
-    });
-    
-  };
-
-  const removeTodoHandler = (id: string) => {
-    setTodos(prev => {
-      return prev.filter(item => item.id !== id);
-    })
-  }
+ 
   return (
     <div>
       <NewTodo onAddTodo={addTodoHandler} />
---
+++ src/store/todos-context.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+import Todo from "../models/todo";
+
+type TodosContextObj = {
+  items: Todo[];
+  addTodo: (text: string) => void;
+  removeTodo: (id: string) => void;
+};
+
+const TodosContext = React.createContext<TodosContextObj>({
+  items: [],
+  addTodo: () => {},
+  removeTodo: (id: string) => {},
+});
+
+const TodosContextProvider: React.FC = (props) => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prev) => {
+      return prev.concat(newTodo);
+    });
+  };
+
+  const removeTodoHandler = (id: string) => {
+    setTodos((prev) => {
+      return prev.filter((item) => item.id !== id);
+    });
+  };
+
+  const contextValue: TodosContextObj = {
+    items: todos,
+    addTodo: addTodoHandler,
+    removeTodo: removeTodoHandler,
+  };
+
+  return (
+    <TodosContext.Provider value={contextValue}>
+      {props.children}
+    </TodosContext.Provider>
+  );
+};
